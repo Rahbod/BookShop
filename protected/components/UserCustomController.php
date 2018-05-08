f7d2b7835bda50f13b3c139426afa8df37f1f56b
Yusef <yusef.mobasheri@gmail.com>
2018-05-08 21:05:20 +0430

    fix bugs

--- protected/components/UserCustomController.php
+++ protected/components/UserCustomController.php
@@ -2,9 +2,8 @@
 
 /**
  * Trait UserCustomController
- * This trat created for custom user functions that accessible in all controllers
- *
- *
+ * This trait created for custom user functions that accessible in all controllers
+ * UserCustomController behaviors similar to classes
  */
 trait UserCustomController{
     /**
